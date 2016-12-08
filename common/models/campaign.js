550b6e327f6c9f7e1e4c5ec045e4e4e1ab70f523
Rithvik Vibhu <rithvikvibhu@gmail.com>
2016-12-08 22:07:31 +0530

    Trying supporters

--- common/models/campaign.js
+++ common/models/campaign.js
@@ -1,8 +1,13 @@
 'use strict';
 
 module.exports = function(Campaign) {
-Campaign.beforeRemote('create', function(context, user, next) {
-  context.args.data.dateCreated = Date.now();
-  context.args.data.customerId = context.req.accessToken.userId;
-});
+
+  Campaign.beforeRemote('create', function(context, user, next) {
+    context.args.data.dateCreated = Date.now();
+    context.args.data.customerId = context.req.accessToken.userId;
+  });
+  // 
+  // Campaign.support = function(campaign, cb) {
+  //
+  // }
 };
